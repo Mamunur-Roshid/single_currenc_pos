eaf0996cec650a78d109b8a1ad91d4ef793e39df
MD MAMUNUR ROSHID <97295022+Mamunur-Roshid@users.noreply.github.com>
2024-01-20 19:10:28 +0600

    Update useConfig.js

--- clientApp/src/composable/useConfig.js
+++ clientApp/src/composable/useConfig.js
@@ -1,7 +1,7 @@
 import { get } from 'lodash';
 
-const host = process.env.NODE_ENV === 'production' ? 'https://pos.skmachineriesbd.com/api' : 'http://127.0.0.1:8000/api';
-const hostName = process.env.NODE_ENV === 'production' ? 'https://pos.skmachineriesbd.com' : 'http://127.0.0.1:8000';
+const host = process.env.NODE_ENV ===  'http://127.0.0.1:8000/api';
+const hostName = process.env.NODE_ENV === 'http://127.0.0.1:8000';
 
 export default function useConfig() {
     return {
